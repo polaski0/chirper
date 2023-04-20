590eae5b76ae5e48982d5e64a56889f058017c6e
Meynard <meynard.trinidad44@gmail.com>
2023-04-20 16:58:34 +0800

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+function App() {
+
+  return (
+    <div>
+      <p className='text-2xl underline font-bold'>Hello World</p>
+    </div>
+  )
+}
+
+export default App
